437e055b165bf03605a36b52b50fb13079d3b261
Gabriella-Kate <gabriellakate04@gmail.com>
2019-10-10 21:58:46 +0200

    connect to db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const db = require('./keys').mongoURI;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(
   bodyParser.urlencoded({
@@ -12,6 +14,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/cities', require('./routes/cities'));
+
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(() => console.log('Connection to Mongo DB established'))
+    .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
