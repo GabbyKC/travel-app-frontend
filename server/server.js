fc8c2b592dedd70ab75ffa570f2543ef5f488588
Gabriella-Kate <gabriellakate04@gmail.com>
2019-10-10 21:41:00 +0200

    initial backend setup

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
+const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const app = express();
 
-app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+app.use(bodyParser.json());
 app.use(cors());
 
-const express = require("express");
-const app = express();
+app.use('/cities', require('./routes/cities'));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
