60051ee983817e7cf840ff5d53e1a35f1a844c53
Gabriella-Kate <gabriellakate04@gmail.com>
2019-10-10 22:36:10 +0200

    testing db retrieval

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const db = require('./keys').mongoURI;
+const db = require("./keys").mongoURI;
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -13,11 +13,16 @@ app.use(
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/cities', require('./routes/cities'));
+app.use("/cities", require("./routes/cities"));
 
-mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-    .then(() => console.log('Connection to Mongo DB established'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connection to Mongo DB established"))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
