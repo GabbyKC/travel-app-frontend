4a546b41035ec594081e7e550810156a0a01c3b9
Gabriella-Kate <gabriellakate04@gmail.com>
2019-10-13 23:40:27 +0200

    cities search bar

--- client/src/screen/Cities.js
+++ client/src/screen/Cities.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
+import React, {Component} from 'react';
+import {connect} from "react-redux";
 import Footer from '../components/Footer/Footer';
 import './Cities.css';
 
-import { getCities } from '../actions';
+import {getCities} from '../actions';
 
 class Cities extends Component {
+    state = {
+        searchTerm: '',
+    };
+
     componentDidMount() {
         this.props.fetchCities();
     }
+
+    doSearchFilter = () => {
+        const convertedSearchTerm = this.state.searchTerm.toLowerCase().replace(/\s/g, "");
+        return this.props.cities.filter(city => city.name.toLowerCase().startsWith(convertedSearchTerm));
+    };
 
     getStyles(city) {
         return {
@@ -18,8 +27,14 @@ class Cities extends Component {
         };
     }
 
+    updateSearchTerm = (e) => {
+        this.setState({
+            searchTerm: e.target.value,
+        })
+    };
+
     render() {
-        const cities = this.props.cities;
+        const cities = this.doSearchFilter();
         const isLoading = this.props.isLoading;
 
         if (isLoading) {
@@ -32,7 +47,7 @@ class Cities extends Component {
             return (
                 <div className='error-bg'>
                     <div className='error-image-container'>
-                        <img className='error-image' src="https://media.giphy.com/media/iic43bLPYAAtbnb6hP/giphy.gif" alt="image broke yo" />
+                        <img className='error-image' src="https://media.giphy.com/media/iic43bLPYAAtbnb6hP/giphy.gif" alt="it be broke yo" />
                     </div>
                     <div className='error-message'>Uh oh... there was an error. Please try again later.</div>
                 </div>
@@ -41,7 +56,14 @@ class Cities extends Component {
 
         return (
             <div>
-                <div>search goes here</div>
+                <div className='input-container'>
+                    <input className="cities-input"
+                        type="text"
+                        placeholder="Search by City.."
+                        value={this.state.searchTerm}
+                        onChange={this.updateSearchTerm}
+                    />
+                </div>
                 <div className='city-list-container'>
                     {cities.map(city => <div className='image-container' style={this.getStyles(city)} key={city._id}> <div className='city-name'>{city.name}</div></div>)}
                 </div>
@@ -53,7 +75,7 @@ class Cities extends Component {
 
 const mapStateToProps = state => {
     return { cities: state.cities.cities, isLoading: state.cities.isLoading };
-}
+};
 
 const mapDispatchToProps = dispatch => ({
     fetchCities: () => dispatch(getCities())
